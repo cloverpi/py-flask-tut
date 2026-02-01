7045cb8f9b838bb0c439bab6a9c46d8cbfc278e4
CloverPi <christopher.humber@gmail.com>
2026-02-01 17:14:53 -0600

    ch2: jinja conditionals

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import flask_app
 
 @flask_app.route('/')
 @flask_app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'username': 'CloverPi'}
+    return render_template('index.html',  user=user)
