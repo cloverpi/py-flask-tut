b59b0d5d1c06834117cdbe82fd79b50158ea8c09
CloverPi <christopher.humber@gmail.com>
2026-02-11 22:38:59 -0600

    ch 3. login routes: get, post and validation

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import flask_app
+from app.forms import LoginForm
 
 @flask_app.route('/')
 @flask_app.route('/index')
@@ -16,3 +17,11 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+@flask_app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('index')
+    return render_template('login.html', title='Login', form=form)
